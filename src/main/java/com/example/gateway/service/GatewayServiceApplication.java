51221ed4bc3eb3475479f5e6d140c22f51949b13
mahima <dodmahima@gmail.com>
2025-09-12 14:07:02 +0530

    logger added

--- src/main/java/com/example/gateway/service/GatewayServiceApplication.java
+++ src/main/java/com/example/gateway/service/GatewayServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.gateway.service;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +11,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		MDC.put("service", "Gateway-service");
 		SpringApplication.run(GatewayServiceApplication.class, args);
-
-
+		@PostConstruct
+		public void initMDC() {
+			MDC.put("service", "authenticator-service");
+		}
 	}
 
 }
